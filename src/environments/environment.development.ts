69fa48bc18eff042e7ef3c70412dcc3ac043fd52
anthony_bernilla <antonybernilla2@gmail.com>
2025-02-24 18:08:39 -0500

    probar subir

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -5,6 +5,7 @@ export const environment = {
   API: HOST + "/cix-tech-mart-api/v1",
   CLAVE_STRIPE:'pk_test_51Qj2SdH9VL1faGtjACW8H2y18sigiesjoJIgvjnoKETf7u5WplC6pDLHCEkHSIlX0PzjdLfJIomcSMT4CAJDqtWi008qiX9c0W',
   production: true,
+  
   firebase: {
     apiKey: "AIzaSyCFqKKX9TH3At_5bRG5VIKrm5jbE5KaeWQ",
     authDomain: "cix-tech-mart-2025.firebaseapp.com",
